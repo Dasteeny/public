5a8a3963b5c5305a1382c307ad74a80ca64df497
Dasteeny <dastan.zhabagin@gmail.com>
2025-02-28 09:57:18 +0500

    ✨  Add functions to extract markdown images and links with corresponding unit tests

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,7 +1,7 @@
 import unittest
 
 from text_node import TextNode, TextType
-from utils import split_nodes_delimiter
+from utils import extract_markdown_images, extract_markdown_links, split_nodes_delimiter
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -88,6 +88,36 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertEqual(
             str(err.exception), f"invalid node {repr(node)}: unclosed delimeter"
+        )
+
+
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_two_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(
+            extract_markdown_links(text),
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+        )
+
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_two_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(
+            extract_markdown_images(text),
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
         )
 
 
