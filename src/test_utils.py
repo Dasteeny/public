34e7c97bb2fef93760efd1267770e1f2b5f31959
Dasteeny <dastan.zhabagin@gmail.com>
2025-02-28 11:35:11 +0500

    ✅  Add unit tests for split_nodes_image function to validate image node splitting

--- src/test_utils.py
+++ src/test_utils.py
@@ -128,6 +128,33 @@ class TestExtractMarkdownImages(unittest.TestCase):
 
 
 class TestSplitNodeImage(unittest.TestCase):
+    def test_split_image(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_image_single(self):
+        node = TextNode(
+            "![image](https://www.example.COM/IMAGE.PNG)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("image", TextType.IMAGE, "https://www.example.COM/IMAGE.PNG"),
+            ],
+            new_nodes,
+        )
+
     def test_split_images(self):
         node = TextNode(
             "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
@@ -178,7 +205,7 @@ class TestSplitNodeImage(unittest.TestCase):
 
 
 class TestSplitNodeLink(unittest.TestCase):
-    def test_split_link(self):
+    def test_split_links(self):
         node = TextNode(
             "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
             TextType.TEXT,
