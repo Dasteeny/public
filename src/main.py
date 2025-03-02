30557db05e99555162ae5b9894b5f62e54068b5e
Dasteeny <dastan.zhabagin@gmail.com>
2025-03-02 23:07:22 +0500

    ✨  Implement public directory management: add functionality to delete and copy static files, and include new CSS styles

--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
-from text_node import TextNode, TextType
+import os
+import shutil
+
+DIR_PATH_STATIC = "./static"
+DIR_PATH_PUBLIC = "./public"
 
 
 def main():
-    text_node = TextNode("This is a text node", TextType.BOLD, "http://www.boot.dev")
-    print(text_node)
+    print("Deleting public directory...")
+    if os.path.exists(DIR_PATH_PUBLIC):
+        shutil.rmtree(DIR_PATH_PUBLIC)
+
+    print("Copying static files to public directory...")
+    copy_all(DIR_PATH_STATIC, DIR_PATH_PUBLIC)
 
 
-main()
+def copy_all(src, dst):
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+
+    for filename in os.listdir(src):
+        from_path = os.path.join(src, filename)
+        to_path = os.path.join(dst, filename)
+        print(f" * {from_path} -> {to_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, to_path)
+        else:
+            copy_all(from_path, to_path)
+
+
+if __name__ == "__main__":
+    main()
